f7cfe5135e80314d2150227553c04f38c27ab7f1
Gurizzo <56281446+Gurizzo@users.noreply.github.com>
2020-02-10 20:46:45 -0300

    add diseño feo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Login from './Component/Login/Login';
 import Registro from './Component/Registro/Registro';
 
 
+
 function App() {
   return (
 
-
+<div className="App">
 <Login></Login>
+</div>
+
+
+
+
   );
 }
 
