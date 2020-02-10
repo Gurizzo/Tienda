1132bc38e5c20b508e1b9b29456f4dba0bb65325
Gurizzo <56281446+Gurizzo@users.noreply.github.com>
2020-02-10 01:17:05 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './Component/Login/Login';
+import Registro from './Component/Registro/Registro';
+
+function App() {
+  return (
+
+<Login></Login>
+  );
+}
+
+export default App;
