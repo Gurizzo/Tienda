fc910cb31986d2c849eb9587b45ef581eb2b9092
Gurizzo <56281446+Gurizzo@users.noreply.github.com>
2020-02-12 19:56:10 -0300

    add Tarea

---
+++ src/Util/Util.js
@@ -0,0 +1,21 @@
+
+
+filterProductsByName(products,name) = () =>{
+
+return products;
+}
+
+addToCart(cart,item) = () =>{
+
+return true;
+}
+
+countCartItems(cart) = () =>{
+
+return count;
+}
+
+countCartTotalAmount(cart) = () =>{
+
+return total;
+}
