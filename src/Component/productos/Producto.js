b5e3ebcfa9147e58f32d239cf0b72600a5cd7bcd
Gurizzo <56281446+Gurizzo@users.noreply.github.com>
2020-03-02 21:19:58 -0300

    Update Producto.js

--- src/Component/productos/Producto.js
+++ src/Component/productos/Producto.js
@@ -32,20 +32,20 @@ render(){
   return(
     <div className="Producto"  >
 
-    <Card style={{ width: '18rem', height:'25rem' }} >
+    <Card style={{ width: '18rem', height:'32rem' }} >
     <Card.Img variant="top" src= {this.state.photo} style={{ width: '18rem', height:'10rem' }} />
     <Card.Body>
         <Card.Title>{this.state.name}</Card.Title>
       <Card.Text>
       {this.state.description}
       </Card.Text>
-
+       <strong>Precio Unitario <p>${this.state.price}</p></strong>
       <InputGroup className="mb-3">
    
-    <strong>Cantidad</strong><input type="number"  aria-describedby="basic-addon1" value={this.state.count} name="count" required onChange={(event)=>{this.handleChange(event,'count')}} />
+    <strong>Cantidad</strong> <input type="number"  aria-describedby="basic-addon1" value={this.state.count} name="count" required onChange={(event)=>{this.handleChange(event,'count')}} />
   </InputGroup>
 
-        <Button variant="primary" onClick={() => onShopClick(this.props.data,this.state.count)}>${this.state.price}</Button>
+        <Button variant="primary" onClick={() => onShopClick(this.props.data,this.state.count)}>Pagar</Button>
     </Card.Body>
     </Card>
 
